e839e618cd716e2c567e62e90da7192a7e57a149
jeffsLM <jeffes.dev@gmail.com>
2023-02-20 21:02:47 -0300

    add app config

--- App.tsx
+++ App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
+
 import { Navigations } from './src/navigations'
 
 const App = () => {
-  return <Navigations />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Navigations />
+    </GestureHandlerRootView>
+  )
 };
 
 export default App;
